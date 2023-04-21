3d8f030ae5798b74bb47fc46756aa94b41ad64c1
Hi9 <1914619958@qq.com>
2023-04-21 14:46:36 +0800

    dispatch 使用松弛求解

--- dispatch.py
+++ dispatch.py
@@ -38,22 +38,20 @@ q = prices
 # [[pij, ], ] 常数
 p = rates
 
-x = model.addVars(m_edge, n_tasks, n_tasks, vtype=GRB.BINARY, name="X_ikj")
+x = model.addVars(m_edge, n_tasks, n_tasks, lb=0, ub=1, vtype=GRB.CONTINUOUS, name="X_ikj")
 model.update()
 
 # C1约束
 for j in range(n_tasks):
     model.addConstr(quicksum(x[i, k, j]
                              for i in range(m_edge)
-                             for k in range(n_tasks)) == 1)
+                             for k in range(n_tasks)) >= 1)
 model.update()
 # C2 约束
 
 for i in range(m_edge):
     for k in range(n_tasks):
         model.addConstr(quicksum(x[i, k, j] for j in range(n_tasks)) <= 1)
-
-print(m_edge, n_tasks)
 
 model.setObjective(quicksum(k * q[i] * p[i][j] * x[i, k, j]
     for k in range(n_tasks)
@@ -63,6 +61,9 @@ model.setObjective(quicksum(k * q[i] * p[i][j] * x[i, k, j]
 
 model.update()
 
+model.setParam('IntFeasTol', 1e-9)
+
+model.setParam("OutputFlag", 0)
 model.Params.LogToConsole=True # 显示求解过程
 model.Params.MIPGap=0.0001 # 百分比界差
 model.Params.TimeLimit=100 # 限制求解时间为 100s
@@ -71,7 +72,7 @@ model.optimize()
 result = [[] for i in range(m_edge)]
 regex = re.compile(r"\[.*\]")
 for v in model.getVars():
-    line = f"{v.varName}：{round(v.x,3)}" 
+    line = f"{round(v.x,3)}" 
     if "0.0" not in line:
         arr = v.varName
         arr = regex.search(arr).group()
